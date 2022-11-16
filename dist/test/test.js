04b6768510ae56fa84307761d67c97d0351b172f
v203 <vuyisamr203@gmail.com>
2022-11-16 15:34:47 +0200

    test rims class and paint class

--- dist/test/test.js
+++ dist/test/test.js
@@ -4,10 +4,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert_1 = __importDefault(require("assert"));
-describe("Test for the decorator pattern", () => {
-    it("Dummy test 1 + 1", () => {
-        let actual = "1";
-        let expected = "1";
-        (0, assert_1.default)(actual, expected);
+const Car_1 = require("../Car");
+const Paint_1 = __importDefault(require("../Paint"));
+const Rims_1 = __importDefault(require("../Rims"));
+describe("Car alterations using the decorator pattern", () => {
+    it("The specs method should return the type of 'Make : BMW'", () => {
+        let bmwStandardEdition = new Car_1.Car();
+        let actual = bmwStandardEdition.specs();
+        let expected = "| Make: BMW ";
+        assert_1.default.strictEqual(actual, expected);
+    });
+    it("The the specs method should return make and paint type of the vehicle", () => {
+        let bmwStandardEdition = new Car_1.Car();
+        let bmwPainted = new Paint_1.default(bmwStandardEdition);
+        let actual = bmwPainted.specs();
+        let expected = " | Chrome paint | Make: BMW  | ";
+        assert_1.default.strictEqual(actual, expected);
+    });
+    it("The specs Method should state that the bmw has 16-inch rims and that the exterior of the car is coated in chrome.", () => {
+        let bmwStandardEdition = new Car_1.Car();
+        let bmwPainted = new Paint_1.default(bmwStandardEdition);
+        let bmwPaintedWithRims = new Rims_1.default(bmwPainted);
+        let actual = bmwPaintedWithRims.specs();
+        let expected = "| 16 inch rims  | Chrome paint | Make: BMW  | |";
+        assert_1.default.strictEqual(actual, expected);
+    });
+    it("The specs method should only return chrome painting .", () => {
+        let icar = { specs() { return ""; } };
+        let ChromePainted = new Paint_1.default(icar);
+        let actual = ChromePainted.specs();
+        let expected = " | Chrome paint  | ";
+        assert_1.default.strictEqual(actual, expected);
+    });
+    it("The specs method should only return 16 inch rims.", () => {
+        let icar = { specs() { return ""; } };
+        let rims = new Rims_1.default(icar);
+        let actual = rims.specs();
+        let expected = "| 16 inch rims |";
+        assert_1.default.strictEqual(actual, expected);
     });
 });
--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,70 @@
 import assert from "assert"
+import { Car } from "../Car";
+
+import Paint from "../Paint";
+import Rims from "../Rims";
+import IcarModification from "../IcarModification";
 
 
-describe("Test for the decorator pattern",()=>{
-    it("Dummy test 1 + 1",()=>{
 
-        let actual:string = "1";
-        let expected:string = "1";
+describe("Car alterations using the decorator pattern", () => {
 
-        assert(actual,expected)
+    it("The specs method should return the type of 'Make : BMW'", () => {
+        let bmwStandardEdition = new Car();
+
+        let actual: string = bmwStandardEdition.specs();
+        let expected: string = "| Make: BMW ";
+
+        assert.strictEqual(actual, expected)
+    })
+
+    it("The the specs method should return make and paint type of the vehicle", () => {
+
+        let bmwStandardEdition = new Car();
+        let bmwPainted = new Paint(bmwStandardEdition)
+
+        let actual: string = bmwPainted.specs();
+        let expected: string = " | Chrome paint | Make: BMW  | ";
+
+        assert.strictEqual(actual, expected)
+    })
+
+    it("The specs Method should state that the bmw has 16-inch rims and that the exterior of the car is coated in chrome.", () => {
+
+        let bmwStandardEdition = new Car();
+        let bmwPainted = new Paint(bmwStandardEdition)
+        let bmwPaintedWithRims = new Rims(bmwPainted)
+
+        let actual: string = bmwPaintedWithRims.specs();
+        let expected: string = "| 16 inch rims  | Chrome paint | Make: BMW  | |";
+
+        assert.strictEqual(actual, expected)
+    })
+
+
+    it("The specs method should only return chrome painting .", () => {
+       
+        let icar:IcarModification = {specs(){return ""}}
+        
+        let ChromePainted = new Paint(icar)
+        
+
+        let actual: string = ChromePainted.specs()
+        let expected: string = " | Chrome paint  | ";
+
+        assert.strictEqual(actual, expected)
+    })
+
+
+    it("The specs method should only return 16 inch rims.", () => {
+       
+        let icar:IcarModification = {specs(){return ""}}        
+        let rims = new Rims(icar)
+        
+        let actual: string = rims.specs()
+        let expected: string = "| 16 inch rims |";
+
+        assert.strictEqual(actual, expected)
     })
 
 
